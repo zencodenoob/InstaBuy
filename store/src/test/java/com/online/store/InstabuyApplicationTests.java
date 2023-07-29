dbf6724092fa5468444cb18bf35659520d754612
Davis Johnson <daviskoluvally@gmail.com>
2023-07-29 09:45:53 +0530

    Adding dependencies

---
+++ store/src/test/java/com/online/store/InstabuyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.online.store;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InstabuyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
